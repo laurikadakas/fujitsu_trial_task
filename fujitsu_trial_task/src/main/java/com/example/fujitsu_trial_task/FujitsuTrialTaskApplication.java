e545cfb1920125e38fd16d8cf0cf74210218f341
laurikadakas <lauri.kadakas@gmail.com>
2024-03-31 02:47:48 +0200

    Initial project creation with Spring Initializr

---
+++ fujitsu_trial_task/src/main/java/com/example/fujitsu_trial_task/FujitsuTrialTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.fujitsu_trial_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuTrialTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujitsuTrialTaskApplication.class, args);
+	}
+
+}
